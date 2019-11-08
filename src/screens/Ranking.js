69e06357eff202012d603cd4abfc0fe59193bad4
lazymelody <49500492+lazymelody@users.noreply.github.com>
2019-11-08 14:17:55 -0800

    Create Ranking.js

---
+++ src/screens/Ranking.js
@@ -0,0 +1,8 @@
+/**
+ * This is the HabitPage screen class file.
+ * @author: Huilin Song, Yiyun Zhang, Lydia Gui, Grace Chen
+ * @since 11.8.2019
+ */
+import React, {Component} from 'react';
+import {FlatList, StyleSheet, SafeAreaView, View, TextInput, Button, Text, Alert} from 'react-native';
+import db from "../base";
