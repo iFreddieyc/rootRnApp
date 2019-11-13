432e475eb664c398f990f36d463d319af35dd29d
lazymelody <49500492+lazymelody@users.noreply.github.com>
2019-11-13 15:51:48 -0800

    Add files via upload

--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -1,8 +1,78 @@
 /**
  * This is the HabitPage screen class file.
- * @author: Huilin Song, Yiyun Zhang, Lydia Gui, Grace Chen
- * @since 11.8.2019
+ * @author Qiuling Chen TODO
+ * @since 11.3.2019
  */
 import React, {Component} from 'react';
 import {FlatList, StyleSheet, SafeAreaView, View, TextInput, Button, Text, Alert} from 'react-native';
 import db from "../base";
+
+var users_friends;
+users_friends = [];
+export default class Ranking extends Component {
+
+    componentDidMount() {
+        db.firestore().collection("users").where("userid", "==", "user_id1")
+            .get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data().userid);
+                users_friends.push(doc.data().friends); //get the users friend's userid
+            });
+        }).catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+
+        for(var i = 0; i < users_friends.length; i++){
+          db.firestore().collection("habits").where("userid", "==", "users_friends[i]")
+              .get().then(function (querySnapshot) {
+                  querySnapshot.forEach(function (doc) {
+                      // doc.data() is never undefined for query doc snapshots
+                      console.log(doc.id, " => ", doc.data().userid);
+                      if(visible == true && )
+                      habits.push(doc.data());
+                  });
+              }).catch(function (error) {
+              console.log("Error getting documents: ", error);
+          });
+        }
+    }
+
+
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text>Hello</Text>
+                <FlatList
+                    data={habits}
+                    renderItem={({ item }) =>
+                        <HabitView
+                            name={item.name}
+                            date={item.startDate}
+                        />
+                    }
+                />
+            </SafeAreaView>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold',
+        alignItems: 'center'
+    },
+    habitStyle: {
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold', alignItems: 'center',
+    }
+});
