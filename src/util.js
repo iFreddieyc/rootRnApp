334e22e84bd0a96f2079f364fff48f3248ec11a6
yunezy <zhangyiyun@yahoo.com>
2019-11-13 23:19:33 -0800

    Update util.js

--- src/util.js
+++ src/util.js
@@ -12,4 +12,27 @@ export default class util{
         let date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
         return date;
     }0
+    // /**
+    // /* 我咋发觉current Date其实是date object呢？ 所以我没写convert 成int 再计算duration
+    // /* 我直接写了个method算两个date的差，method看的网上的reference
+    //  * Function to convert string mm/dd/yyyy to int yyyymmdd
+    //  */
+    // static DateToInt(parameter){
+    //     var replaced = parameter.replace (/\//g, "");//replace slash with empty string
+    //     var year = replaced.slice(4,7);
+    //     var dayMonth = replaced.slice(0,3);
+    //     var res = year.concat(dayMonth);
+    //     return Number(res); //convert the replaced string to int
+    // }
+    /**
+     * method to get duration between two Dates
+     * p2 must be later than p1!
+     * @param {*} p1 habit start date
+     * @param {*} p2 current date
+     */
+    static getDuration(p1, p2){
+        var diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays;
+    }
 }
