ed971069a711c56d023e0123ebd22b97d3ceefb7
Qingcheng You <q3you@ucsd.edu>
2019-11-17 00:21:15 -0800

    Fixed issue in App.js

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
-import React from 'react';
+/**
+ * Entry point of the application
+ * @author Qingcheng You
+ */
+import React, {Component}from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-//import { createStackNavigator} from "react-navigation-stack";
-import { SignedOut, SignedIn } from "./src/router";
-import Loading from "./src/authFlow/Loading";
-// export default function App() {
-//   return(
-//       <HabitPage />
-//   );
-// }
+import { AppNavigator} from "./src/router";
+import {createAppContainer} from "react-navigation";
 
-// const AppStack = createStackNavigator({ Habit: HabitPage, Ranking: Ranking });
-// const AuthStack = createStackNavigator({ SignIn: SignIn , SignUp: SignUp});
-
-export default createAppContainer(
-    createSwitchNavigator({
-      AuthLoading: Loading,
-      App: SignedIn,
-      Auth: SignedOut,
-    },{
-      initialRouteName: 'AuthLoading',
-    })
-);
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
