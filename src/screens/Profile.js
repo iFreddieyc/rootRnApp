3da1ad53bda59ca8442dd5ac7ae528ec92966a45
mulei123 <38970244+mulei123@users.noreply.github.com>
2019-11-17 00:18:16 -0800

    Update Profile.js

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,6 @@
 /**
  * This is the placeholder for the profile screen
- * @author Qingcheng You TODO
+ * @author Mufan Lei TODO
  * @since 11.8.2019
  */
 import React, {Component} from 'react';
@@ -21,6 +21,7 @@ export default class Profile extends Component {
         var email;
         var phoneNumber;
         var picUrl;
+        var url = "gs://rootappcse110ntl.appspot.com" + picUrl;
         userRef.get().then(function(doc) {
           userName = doc.data().userName;
           email = doc.data().email;
@@ -29,7 +30,7 @@ export default class Profile extends Component {
         })
         return(
             <View style={styles.container}>
-                <img src="gs://rootappcse110ntl.appspot.com" + picUrl;/>
+                <img src={url}/>
                 <Text>Profile</Text>
                 <Text>userName</Text>
                 <Text>email</Text>
