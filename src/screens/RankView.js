e02b498b3d3f9bd18a0b66d7444cf95ae61eec4f
iFreddieyc <34200232+iFreddieyc@users.noreply.github.com>
2019-11-19 18:22:53 -0800

    Fixed async in ranking.js.
    Still need to order and limit habits

---
+++ src/screens/RankView.js
@@ -0,0 +1,78 @@
+/**
+ * This file renders the rank component onto ranking page
+ * @author Yiyun Zhang, Yining Chen, Lydia Gui
+ * @since 11.8.2019
+ */
+import React, {Component} from 'react';
+import {
+    FlatList,
+    StyleSheet,
+    View,
+    TextInput,
+    Button,
+    Text,
+    Modal,
+    TouchableHighlight,
+    TouchableOpacity
+} from 'react-native';
+
+export default class RankView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <TouchableHighlight style={styles.container}
+            >
+                <View>
+                    <Text style={styles.authorName}>
+                        {this.props.authorName}
+                    </Text>
+                    <Text style={styles.habitName}>
+                        {this.props.habitName}
+                    </Text>
+                    <Text style={styles.habitDuration}>
+                        {this.props.duration}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        );
+
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //alignItems: 'center',
+        marginTop: '30%',
+        marginBottom: '30%',
+        width: '100%',
+        height: '30%',
+        //padding: 100,
+        backgroundColor: 'cyan',
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+    },
+    authorName: {
+        fontSize: 14,
+        color: 'pink',
+        fontWeight: 'bold',
+        alignItems: 'center'
+    },
+    habitName: {
+        fontSize: 14,
+        color: 'pink',
+        fontWeight: 'bold',
+        alignItems: 'center'
+    },
+    habitDuration: {
+        fontSize: 14,
+        color: 'black',
+        fontWeight: 'bold',
+        alignItems: 'center',
+    }
+});
--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -1,18 +1,145 @@
 /**
- * This is the placeholder for the ranking screen
- * @author Qingcheng You TODO
+ * This is the Ranking screen class file.
+ * @author Yiyun Zhang, Yining Chen, Lydia Gui, Qingcheng You
  * @since 11.8.2019
  */
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    FlatList,
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    ActivityIndicator
+} from 'react-native';
+
+import db from "../base";
+import RankView from "./RankView";
+import Habit from "../Habit";
+import util from "../util";
 
 export default class Ranking extends Component {
-    render(){
-        return(
-            <View sytle={styles.container}>
-                <Text>Ranking</Text>
-            </View>
-        )
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            friends: [],
+            ranking: [],
+            message: "Add a friend to see their progress!"
+        };
+    }
+
+    componentDidMount() {
+        this.getRanking();
+    }
+
+    getRanking() {
+        this.getFriendList().then((friendsList) => {
+            this.setState({
+                friends: friendsList,
+            });
+            const arr = [];
+            for (const id of friendsList) {
+                console.log(id);
+                arr.push(this.getHabitFromUid(id));
+            }
+            Promise.all(arr).then((values) => {
+                values = values.filter(
+                    item => item !== undefined
+                );
+                values.sort(util.compare);
+                //console.log("Done");
+                //console.log(values);
+                this.setState({
+                    ranking: values,
+                    isLoading: false,
+                });
+                if(values.length > 0){
+                    this.setState({
+                        message: "HabitRank",
+                    });
+                }
+                console.log(values);
+            }).catch(function (error) {
+                    console.log("Something went wrong", error);
+                }
+            );
+        });
+    }
+
+    getFriendList = () => {
+        return new Promise((resolve, reject) => {
+            let friendList = [];
+            db.firestore().collection("users")
+                .where("userid", "==", db.auth().currentUser.uid)
+                .get()
+                .then(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        // doc.data() is never undefined for query doc snapshots
+                        friendList = doc.data().friends;
+                        console.log(friendList);
+                    });
+                    resolve(friendList);
+                })
+                .catch(function (error) {
+                    console.log("Inside getFriendList, error getting documents: ", error);
+                });
+        })
+    }
+
+    getHabitFromUid = (uid) => {
+        let habit;
+        return new Promise((resolve, reject) => {
+            db.firestore().collection("habits")
+                .where("visible", "==", true).where("userid", "==", uid)
+                //.orderBy("name", "asc")
+                .get()
+                .then(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        if (doc.exists) {
+                            habit = new Habit(doc.data().name, doc.data().userid,
+                                doc.data().startDate, doc.data().description, doc.data().visible);
+                            console.log(habit);
+                        }
+                    });
+                    resolve(habit);
+                })
+                .catch(function (error) {
+                    console.log("Inside getHabitFromUid, error getting documents: ", error);
+                });
+        });
+
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            console.log("Hello");
+            return (
+                <View style={styles.activity}>
+                    <ActivityIndicator size="large" color="#0000ff"/>
+                </View>
+            )
+        } else {
+            return (
+                <SafeAreaView style={styles.container}>
+                    <Text style={styles.title}>
+                        {this.state.message}
+                    </Text>
+                    <FlatList
+                        data={this.state.ranking}
+                        renderItem={({item}) =>
+                            <RankView
+                                authorName={item.userid}
+                                habitName={item.name}
+                                duration={item.duration}
+                            />
+                        }
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </SafeAreaView>
+            );
+        }
     }
 }
 
@@ -20,8 +147,27 @@ export default class Ranking extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center'
     },
+    title: {
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold',
+        alignItems: 'center'
+    },
+    habitStyle: {
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold', alignItems: 'center',
+    },
+    activity: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
