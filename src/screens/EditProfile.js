67c3a62305fe74196d326d5987577e43fa443e7d
lazymelody <49500492+lazymelody@users.noreply.github.com>
2019-11-24 19:37:49 -0800

    Update EditProfile.js

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -4,20 +4,24 @@
  * @since 11.8.2019
  */
 import React, {Component} from 'react';
-import {FlatList, StyleSheet, SafeAreaView, View, TextInput, Button, Text, Alert} from 'react-native';
+import {FlatList, StyleSheet, SafeAreaView, View, TextInput, Button, Text, Alert, Camera} from 'react-native';
 import db from "../base";
  import * as firebase from 'firebase/app';
+ import * as ImagePicker from 'react-native-image-picker';
+
 
 export default class EditProfile extends Component {
 
-  /**
+   /**
      * Constructor for class SignUp
      * @param props properties to initialize this class with
      */
     constructor(props) {
         super(props);
-        this.state = {name: '', phoneNum: -1, password: ''};
+        this.state = {name: '', password: '', filePath: ''};
     }
+
+
 
     /**
      * Handles text input
@@ -28,12 +32,50 @@ export default class EditProfile extends Component {
         this.setState({[key]: val});
     }
 
+
+    handleChoosePhoto = () => {
+      const options = {
+        noData: true,
+      };
+      ImagePicker.launchImageLibrary(options, response => {
+        if (response.uri) {
+          this.setState({ filePath: response });
+        }
+      });
+    };
+
+
+    componentDidMount() {
+       this.getPermissionAsync();
+       console.log('hi');
+     }
+
+     getPermissionAsync = async () => {
+       if (Constants.platform.ios) {
+         const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+         if (status !== 'granted') {
+           alert('Sorry, we need camera roll permissions to make this work!');
+         }
+         else{
+           console.log("got permission");
+         }
+       }
+     }
+
+
+
+
+
+
+
+
+
+
     saveProfile = () => {
       const {name, phoneNum, password} = this.state;
       var userRef = db.firestore().collection('users').doc(db.auth().currentUser.uid);
       userRef.update({
-        userName: name,
-        phoneNumber: phoneNum
+        userName: name
         });
     }
 
@@ -46,27 +88,22 @@ export default class EditProfile extends Component {
                            autoCapitalize={"none"}
                            onChangeText={val => this.onChangeText('name', val)}
                 />
-                <Text>PhoneNum:</Text>
-                <TextInput style={styles.input}
-                           placeholder="phoneNum"
-                           secureTextEntry={true}
-                           autoCapitalize={"none"}
-                           onChangeText={val => this.onChangeText('phoneNum', val)}
-                />
                 <Button
                     title={"Save Change"}
                     onPress={this.saveProfile}
                 />
                 <Button
                     title={"Upload Picture"}
-                    onchange={(e)=>{
-                      var file = e.target.files[0];
-                      var filePath = 'pics/'+file.name;
+                    onPress={(e)=>{
+                      //var file = e.target.files[0];
+                      //var filePath = 'pics/'+file.name;
+                      this.handleChoosePhoto();
+                      /*var filePath = this.state.filePath;
                       var storageRef = firebase.storage().ref(filePath);
                       var task = storageRef.put(file);
                       var usersRef = db.firestore().collection('users');
                       var userRef = usersRef.where('userid', '==', db.auth().currentUser.uid);
-                      userRef.update({userPicUrl:filePath});
+                      userRef.update({userPicUrl:filePath});*/
                       }}
                 />
                 <TextInput style={styles.input}
@@ -77,11 +114,14 @@ export default class EditProfile extends Component {
                 />
                 <Button
                     title={"Save New Password"}
-                    onchange={(e)=>{
+                    onPress={(e)=>{
                       const {name,phoneNum,password} = this.state;
+                      console.log(password);
                       firebase.auth().currentUser.updatePassword(password);
                       }}
                 />
+                <Camera  ref={cam => {this.camera = cam}}  style={styles.preview}  aspect={Camera.constants.Aspect.fill}>  <Text style={styles.capture} onPress={this.takePicture.bind(this)}>    [CAPTURE]  </Text></Camera>
+
             </View>
         );
   }
